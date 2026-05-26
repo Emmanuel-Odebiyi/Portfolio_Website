815dd4146b224430eeec33e10d18df797273e3c4
Emmanuel Odebiyi <emmanuelodebiyiwrites@gmail.com>
2026-05-27 00:47:47 +0100

    feat: initialize TinaCMS configuration and generate core schema types

--- tina/__generated__/types.js
+++ tina/__generated__/types.js
@@ -110,7 +110,7 @@ const generateRequester = (client) => {
 export const ExperimentalGetTinaClient = () => getSdk(
   generateRequester(
     createClient({
-      url: "http://localhost:4001/graphql",
+      url: "https://content.tinajs.io/2.4/content/dummy/github/master",
       queries
     })
   )
